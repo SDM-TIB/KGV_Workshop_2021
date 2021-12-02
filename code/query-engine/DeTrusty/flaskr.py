85a3052a05735ccbe948e2a6b10b7f536a8da6d1
Philipp Rohde <Philipp.Rohde@tib.eu>
2021-12-02 18:52:28 +0100

    enable caching

--- code/query-engine/DeTrusty/flaskr.py
+++ code/query-engine/DeTrusty/flaskr.py
@@ -217,13 +217,5 @@ def serve_configure():
     return render_template('configure.html', success=None)
 
 
-@app.after_request
-def no_cache(r):
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    return r
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
